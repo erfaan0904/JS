569d063cefdaa6c3cdaa04049b0ebe6aeb30a362
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2024-06-29 10:47:05 +0330

    Add files via upload

---
+++ RandonNumberGenerator.js
@@ -0,0 +1,9 @@
+for(var i = 1; i <= 10; i++)
+{
+    var rn = Math.floor(Math.random() * 100000); // rn : Random Number
+    console.log(i + ' . ' + rn)
+}
+console.log("---------------------------------")
+
+var rn = Math.floor(Math.random() * 100000); // rn : Random Number
+console.log("your captcha is : ", rn)
