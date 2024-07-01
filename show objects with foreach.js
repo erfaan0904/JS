472de2a05b7d451a819009f59cd400cfb1aeff33
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2024-07-01 14:08:18 +0330

    Add files via upload

---
+++ show objects with foreach.js
@@ -0,0 +1,7 @@
+var users = [
+    { id : 1, name : "alex",   lastname : "mercer", age : 24},
+    { id : 2, name : "artor",  lastname : "morgan", age : 30},
+    { id : 3, name : "thomas", lastname : "shelby", age : 18},
+]
+users.forEach(function(user)
+{  console.log("name      : " + user.name + '\n' + "last name : " + user.lastname)  })
