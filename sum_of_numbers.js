e931b79d26d16b06c3f40c14884211a930d44c4a
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2024-07-12 15:57:35 +0330

    Update sum_of_numbers.js

--- sum_of_numbers.js
+++ sum_of_numbers.js
@@ -1,3 +1,4 @@
+// you can give a number to this code and this code will show you sum of 0 to that number
 let num = prompt("enter a number : ")
 let sum = 0
 for(var i = 1; i <= num; i++)  {  sum += i  }
